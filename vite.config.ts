63fc76b071c11a96a760cbd33c476bd0811e63a4
hejianer <jianer.he@shopline.com>
2024-11-28 11:46:21 +0800

    chorm: 修复启动错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     modules: {
       localsConvention: 'camelCase', // 可选：将类名转换为驼峰命名
+    },
+  },
+  server: {
+    hmr: {
+      overlay: false, // 禁用 HMR 错误覆盖
     },
   },
 })
